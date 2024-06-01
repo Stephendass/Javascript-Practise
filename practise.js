15c5eb7803d697ce94de39df88069c8493fa56a0
Stephendass <stephen.stainluss@gmail.com>
2024-06-01 21:03:58 +0530

    first commit

---
+++ practise.js
@@ -0,0 +1,55 @@
+//Eg1
+var price = 5000000;
+var product = 'BMW'
+const tax = 18;
+
+// console.log(product)
+// var total = price + tax;
+
+// console.log(total)
+// Eg2
+
+var fruitName = "Apple"
+var count = 2
+var priceOfFruit = 100;
+
+var total = count * priceOfFruit;
+
+console.log(fruitName)
+console.log(total)
+
+let x = 5;
+
+console.log(x);
+let y = ++x;
+
+
+console.log(x);
+console.log(y);
+
+var a = 10;
+var b = 20;
+function add(){
+    return a + b;
+}
+add();
+// let result = add();
+console.log(add());
+
+
+var favActor = "Kamal Hasan";
+var favPlayer = "MS Dhoni";
+var movie = "Anbe Sivam";
+function favDetails(){
+    console.log("Favourite Actor: " + favActor)
+    console.log("Favourite Player: " + favPlayer)
+    console.log("Favourite Movie: " + movie)
+}
+favDetails();
+
+
+function area(a, b) {
+    console.log(a*b);
+}
+
+area(10,20)
